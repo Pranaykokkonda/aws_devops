64583ba61743aa7082f75c9dffb3f7435bbb9916
Pranaykokkonda <pranay.kokkonda22@gmail.com>
2025-06-26 00:19:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -49,7 +49,7 @@ variable "github_username" {
 variable "github_token" {
   description = "GitHub token for private repo access"
   type        = string
-  default     = "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"
+  default     = "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"
   sensitive   = true
 }
 
